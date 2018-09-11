34fc43b4e31478c0625524969b2c0126f9f9c46e
Elias Mikkola <elias.mikkola1@gmail.com>
2018-09-11 10:12:29 +0300

    4.4 apufunktioita ja yksikkötestejä, osa 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1
 }
-  
+
+const totalLikes = (blogs) => {
+    if(blogs.length === 0) return 0
+    
+    const reducer = (x,y) => x + y;
+    return blogs.map(n=>n.likes).reduce(reducer)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
