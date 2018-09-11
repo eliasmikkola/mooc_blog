fb7ecf78b08c03e02be74fec227bb891a15e7a34
Elias Mikkola <elias.mikkola1@gmail.com>
2018-09-11 17:53:19 +0300

    4.12*, added describe for GET and POST testing sections, added helper function for GETting blogs in db after POST

--- tests/blog.api.test.js
+++ tests/blog.api.test.js
@@ -5,6 +5,12 @@ const testData = require('../utils/blog-list')
 
 const Blog = require('../models/blog')
 
+const blogsInDb = async () => {
+    const blogs = await api
+        .get('/api/blogs')
+    return blogs
+}
+
 beforeAll(async () => {
     await Blog.remove({})
     
@@ -15,108 +21,107 @@ beforeAll(async () => {
 })
 
 
-test('notes are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe('GET api tests', () => {
 
-test('all blogs are returned', async () => {
-    const response = await api
+    test('blogs are returned as json', async () => {
+    await api
         .get('/api/blogs')
-    expect(response.body.length).toBe(testData.length)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+
+    test('all blogs are returned', async () => {
+        const response = await blogsInDb()
+        expect(response.body.length).toBe(testData.length)
+    })
+
+    test('a specific blog is within the returned blogs', async () => {
+        const response = await blogsInDb()
+    
+        const contents = response.body.map(r => r.title)
+    
+        expect(contents).toContain('Go To Statement Considered Harmful')
+    })
 })
+describe('POST api tests', () => {
 
-test('a specific blog is within the returned notes', async () => {
-    const response = await api
-        .get('/api/blogs')
-  
-    const contents = response.body.map(r => r.title)
-  
-    expect(contents).toContain('Go To Statement Considered Harmful')
-})
+    test('a valid blog post can be added ', async () => {
+        const newBlog = {
+            title: "Cats in Computer Science",
+            author: "Cat Stevens",
+            url: "https://http.cat/",
+            likes: 11,
+            __v: 0
+        }
+    
+        await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+        const response = await blogsInDb()
+    
+        const contents = response.body.map(r => r.title)
+    
+        expect(response.body.length).toBe(testData.length + 1)
+        expect(contents).toContain(newBlog.title)
+    })
 
+    test('if property "likes" has no value, should be assigned a value of 0', async () => {
+        const newBlogWithoutLikes= {
+            title: "Dogs in Computer Science",
+            author: "DOg Stevens",
+            url: "https://http.cat/",
+            __v: 0
+        }
+    
+        await api
+        .post('/api/blogs')
+        .send(newBlogWithoutLikes)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+        const response = await blogsInDb()
+    
+        const contents = response.body.map(r => r.title)
+        const lastElement = response.body[response.body.length - 1]
 
-test('a valid blog post can be added ', async () => {
-    const newBlog = {
-        title: "Cats in Computer Science",
-        author: "Cat Stevens",
-        url: "https://http.cat/",
-        likes: 11,
-        __v: 0
-      }
-  
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-  
-    const response = await api
-      .get('/api/blogs')
-  
-    const contents = response.body.map(r => r.title)
-  
-    expect(response.body.length).toBe(testData.length + 1)
-    expect(contents).toContain(newBlog.title)
-})
-
-test('if property "likes" has no value, should be assigned a value of 0', async () => {
-    const newBlogWithoutLikes= {
-        title: "Dogs in Computer Science",
-        author: "DOg Stevens",
-        url: "https://http.cat/",
-        __v: 0
-      }
-  
-    await api
-      .post('/api/blogs')
-      .send(newBlogWithoutLikes)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-  
-    const response = await api
-      .get('/api/blogs')
-  
-    const contents = response.body.map(r => r.title)
-    const lastElement = response.body[response.body.length - 1]
-
-    expect(lastElement.likes).toBe(0)
-})
+        expect(lastElement.likes).toBe(0)
+    })
 
 
-test('POSTs missing properties "title" and "url" should return 400', async () => {
-    const newBlogWithoutUrl= {
-        title: 'Dogs in Computer Science',
-        author: 'Dog Stevens',
-        __v: 0
-    }
-    const newBlogWithoutTitle= {
-        url: 'https://http.cat',
-        author: 'Dog Stevens',
-        __v: 0
-    }
-  
-    await api
-      .post('/api/blogs')
-      .send(newBlogWithoutTitle)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
+    test('POSTs missing properties "title" and "url" should return 400', async () => {
+        const newBlogWithoutUrl= {
+            title: 'Dogs in Computer Science',
+            author: 'Dog Stevens',
+            __v: 0
+        }
+        const newBlogWithoutTitle= {
+            url: 'https://http.cat',
+            author: 'Dog Stevens',
+            __v: 0
+        }
+    
+        await api
+        .post('/api/blogs')
+        .send(newBlogWithoutTitle)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-    await api
-      .post('/api/blogs')
-      .send(newBlogWithoutUrl)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
-  
-    const response = await api
-      .get('/api/blogs')
-  
-    const contents = response.body.map(r => r.title)
-    const lastElement = response.body[response.body.length - 1]
+        await api
+        .post('/api/blogs')
+        .send(newBlogWithoutUrl)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+    
+        const response = await blogsInDb()
+    
+        const contents = response.body.map(r => r.title)
+        const lastElement = response.body[response.body.length - 1]
 
-    expect(lastElement.likes).toBe(0)
+        expect(lastElement.likes).toBe(0)
+    })
 })
 
 
